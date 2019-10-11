c5059d9ad3913473a310de6b83fe9eb4794f6b1b
Richard <r.mora@students.clark.edu>
2019-10-10 23:47:54 -0700

    Programming assignment 1 part 2 complete

---
+++ Programming assignment 1 part 2/Prob2/Program.cs
@@ -0,0 +1,79 @@
+﻿/*
+Author: Richard Mora
+Date: 10/9/2019
+CTEC 135: Microsoft Software Development with C#
+
+Module 2, Programming Assignment 1, problem 2 Iteration Constructs
+
+region 1: print 1-5 with spaces in between on a single line using a for loop
+
+region 2: print 1-5 with spaces in between on a single line using a while loop
+
+region 3: print 1-5 with spaces in between on a single line using a do/while loop
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob2
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //Input: Create objects that is an integer data type, each has a value of 1 while inside 3 different loops.
+
+            //Output: For the 3 loops when executed, each will print out the numbers 1 through 5 in a single line with spaces in between each number.
+
+            //Behavior: Each loop will have an object with a value of 1 (i, count for example), as long as that value is
+            //less than 6 the loop will execute over and over again while adding 1 to that value (1 becomes 2 and starts 
+            //loop over again). This will continue until each object is no longer less than 6 then the loop ends. 
+        {
+            // this region name is for loop
+            #region for loop
+            // this creates a for loop where we create an object i with a int data type that has a value of 1. The condition is as long i is less than 6 the loop will 
+            //contiune to run and increase by one each time the loop executes until the condition is no longer true.
+            for (int i = 1; i < 6; i++)
+            {
+                // Console.Write() prints out anything inside () on a single line. In this case, we print out the value of i following a space (ex: 1 2...)
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+            #endregion
+
+            // this region name is while loop
+            #region while loop
+            // we start by creating an object Count that has a int data type and give it a value of 1.
+            int Count = 1;
+            // we start a while loop where as long as Count is less than 6, the loop will contiune to execute until the condition is no longer true.
+            while (Count < 6)
+            {
+                // Same as with the for loop, Console.Write() prints out anything inside (). in this case, prints out the value of Count following a space in one line.
+                Console.Write("{0} ", Count);
+
+                // After the while loop execute a loop, we use Count++ to add 1 to the value of Count. This will cause the value of Count to increase in value until it becomes 6 which cause the condition above to be false otherwise the while loop will keep executing causing a infinite loop. 
+                Count++;
+            }
+            Console.WriteLine();
+            #endregion
+
+            // this region name is do/while loop
+            #region do/while loop
+            // we start by creating an int data type count with a value of 1
+            int count = 1;
+            // the do loop will execute the code inside the {} as long as the condition in the while line is no longer true
+            do
+            {
+                // we use console write to print count on a single line with a space in between 
+                Console.Write("{0} ", count);
+                // at the end of each loop we add 1 to the value of count
+                count++;
+            }
+            // the while line is the condition to the do loop, as long as this condition is true, the do/while loop will continue to execute.
+            while (count < 6);
+            Console.WriteLine();
+            #endregion
+        }
+    }
+}
---
+++ Programming assignment 1 part 2/Prob3/Program.cs
@@ -0,0 +1,240 @@
+﻿/*
+Author: Richard Mora
+Date: 10/10/2019
+CTEC 135: Microsoft Software Development with C#
+
+Module 2, Programming Assignment 1, problem 3 Decision Constructs
+
+region 1: Create three nested loop one for each of the three conditions. 
+          Using if statements to print out all possible actions base on the conditions
+
+region 2: Create a switch statement inside a for loop which goes 
+     through the number 1 to 7, every possible combination of conditions.
+          Using cases to print out all possible actions base on each number
+     of the for loop.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob3
+{
+    class Program
+    {
+        static void Main(string[] args)
+            // Input: Create 3 nested loops, if statements, switch statement with a for loop for the
+            // possible conditions. Create 5 methods to represent all 5 actions depending on one or more conditions
+
+            // Output: Print the 3 conditions and tell us whether they are true or false for all three.
+            // Print out the necessary actions depending on the state of the conditions
+
+            // Behavior: Each region (if and switch statements) will give us a value 1 to 7 and will print 
+            // out the necessary actions to take base on the numbers which represents all possible
+            // combinations of all three statements and if they're true or false.
+        {
+            #region region 1
+            Console.WriteLine("Using if statements\n");
+            for (int cond1 = 0; cond1 <= 1; cond1++)
+            {
+                for (int cond2 = 0; cond2 <= 1; cond2++)
+                {
+                    for (int cond3 = 0; cond3 <= 1; cond3++)
+                    {
+                        if (cond1 == 0 && cond2 == 0 && cond3 == 0)
+                        {
+                            Console.WriteLine("Printer prints: False");
+                            Console.WriteLine("Red light flashing: False");
+                            Console.WriteLine("Printer is recognized: False");
+                            Console.WriteLine("---------------------");
+                            PowerCable();
+                            PrintComputerCable();
+                            PrinterSoftware();
+                            Console.WriteLine();
+                        }
+                        
+                        if (cond1 == 0 && cond2 == 0 && cond3 == 1)
+                        {
+                            Console.WriteLine("Printer prints: False");
+                            Console.WriteLine("Red light flashing: False");
+                            Console.WriteLine("Printer is recognized: True");
+                            Console.WriteLine("---------------------");
+                            PaperJam();
+                            Console.WriteLine();
+                        }
+                        if (cond1 == 0 && cond2 == 1 && cond3 == 0)
+                        {
+                            Console.WriteLine("Printer prints: False");
+                            Console.WriteLine("Red light flashing: True");
+                            Console.WriteLine("Printer is recognized: False");
+                            Console.WriteLine("---------------------");
+                            PrintComputerCable();
+                            PrinterSoftware();
+                            CheckInk();
+                            Console.WriteLine();
+                        }
+                        if (cond1 == 0 && cond2 == 1 && cond3 == 1)
+                        {
+                            Console.WriteLine("Printer prints: False");
+                            Console.WriteLine("Red light flashing: True");
+                            Console.WriteLine("Printer is recognized: True");
+                            Console.WriteLine("---------------------");
+                            CheckInk();
+                            PaperJam();
+                            Console.WriteLine();
+                        }
+                        if (cond1 == 1 && cond2 == 0 && cond3 == 0)
+                        {
+                            Console.WriteLine("Printer prints: True");
+                            Console.WriteLine("Red light flashing: False");
+                            Console.WriteLine("Printer is recognized: False");
+                            Console.WriteLine("---------------------");
+                            PrinterSoftware();
+                            Console.WriteLine();
+                        }
+                        if (cond1 == 1 && cond2 == 0 && cond3 == 1)
+                        {
+                            Console.WriteLine("Printer prints: True");
+                            Console.WriteLine("Red light flashing: False");
+                            Console.WriteLine("Printer is recognized: True");
+                            Console.WriteLine("---------------------");
+                            Console.WriteLine("No actions are required");
+                            Console.WriteLine();
+                        }
+                        if (cond1 == 1 && cond2 == 1 && cond3 == 0)
+                        {
+                            Console.WriteLine("Printer prints: True");
+                            Console.WriteLine("Red light flashing: True");
+                            Console.WriteLine("Printer is recognized: False");
+                            Console.WriteLine("---------------------");
+                            PrinterSoftware();
+                            Console.WriteLine();
+                        }
+                        if (cond1 == 1 && cond2 == 1 && cond3 == 1)
+                        {
+                            Console.WriteLine("Printer prints: True");
+                            Console.WriteLine("Red light flashing: True");
+                            Console.WriteLine("Printer is recognized: True");
+                            Console.WriteLine("---------------------");
+                            CheckInk();
+                            Console.WriteLine("\n\n");
+                       
+                        }
+                    }
+                }
+            }
+            #endregion
+
+            #region region 2
+            Console.WriteLine("Using switch statements\n");
+            for (int i = 0; i <= 7; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        Console.WriteLine("Printer prints: False");
+                        Console.WriteLine("Red light flashing: False");
+                        Console.WriteLine("Printer is recognized: False");
+                        Console.WriteLine("---------------------");
+                        PowerCable();
+                        PrintComputerCable();
+                        PrinterSoftware();
+                        Console.WriteLine();
+                        break;
+
+                    case 1:
+                        Console.WriteLine("Printer prints: False");
+                        Console.WriteLine("Red light flashing: False");
+                        Console.WriteLine("Printer is recognized: True");
+                        Console.WriteLine("---------------------");
+                        PaperJam();
+                        Console.WriteLine();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Printer prints: False");
+                        Console.WriteLine("Red light flashing: True");
+                        Console.WriteLine("Printer is recognized: False");
+                        Console.WriteLine("---------------------");
+                        PrintComputerCable();
+                        PrinterSoftware();
+                        CheckInk();
+                        Console.WriteLine();
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Printer prints: False");
+                        Console.WriteLine("Red light flashing: True");
+                        Console.WriteLine("Printer is recognized: True");
+                        Console.WriteLine("---------------------");
+                        CheckInk();
+                        PaperJam();
+                        Console.WriteLine();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Printer prints: True");
+                        Console.WriteLine("Red light flashing: False");
+                        Console.WriteLine("Printer is recognized: False");
+                        Console.WriteLine("---------------------");
+                        PrinterSoftware();
+                        Console.WriteLine();
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Printer prints: True");
+                        Console.WriteLine("Red light flashing: False");
+                        Console.WriteLine("Printer is recognized: True");
+                        Console.WriteLine("---------------------");
+                        Console.WriteLine("No actions is required");
+                        Console.WriteLine();
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Printer prints: True");
+                        Console.WriteLine("Red light flashing: True");
+                        Console.WriteLine("Printer is recognized: False");
+                        Console.WriteLine("---------------------");
+                        PrinterSoftware();
+                        Console.WriteLine();
+                        break;
+
+                    case 7:
+                        Console.WriteLine("Printer prints: True");
+                        Console.WriteLine("Red light flashing: True");
+                        Console.WriteLine("Printer is recognized: True");
+                        Console.WriteLine("---------------------");
+                        CheckInk();
+                        Console.WriteLine();
+                        break;
+                }
+            }
+            #endregion
+        }
+        static void CheckInk()
+        {
+            Console.WriteLine("Check ink and replace if empty.");
+        }
+
+        static void PaperJam()
+        {
+            Console.WriteLine("Check printer for paper jam.");
+        }
+
+        static void PowerCable()
+        {
+            Console.WriteLine("Check to ensure power cable is connected.");
+        }
+
+        static void PrintComputerCable()
+        {
+            Console.WriteLine("Check to ensure cable connecting printer and computer are connected.");
+        }
+
+        static void PrinterSoftware()
+        {
+            Console.WriteLine("Ensure the printer software is installed.");
+        }
+    }
+}
